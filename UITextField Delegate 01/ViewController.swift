c9a0e81463b44817dd604ab902638910bff9ca1f
왕휘재 | Hwijae Wang <37130260+adad9900@users.noreply.github.com>
2018-03-28 12:56:09 +0900

    Update ViewController.swift

--- UITextField Delegate 01/ViewController.swift
+++ UITextField Delegate 01/ViewController.swift
@@ -1,29 +1,49 @@
 //
 //  ViewController.swift
-//  UITextField Delegate 01
+//  UITextField Deligate01
 //
-//  Created by D7703_15 on 2018. 3. 22..
-//  Copyright © 2018년 D7703_15. All rights reserved.
+//  Created by D7702_10 on 2018. 3. 22..
+//  Copyright © 2018년 ksh. All rights reserved.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var txt: UILabel!
-    @IBOutlet weak var txtField: UITextField!
+class ViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet weak var lab: UILabel!
+    @IBOutlet weak var labs: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        labs.delegate = self
+        labs.placeholder = "입력을 하세요!"
+        labs.clearButtonMode = UITextFieldViewMode.whileEditing
         
-        txtField.placeholder = "입력을 하세요"
-        txtField.clearButtonMode = UITextFieldViewMode.whileEditing //클리어 버튼
+    }
+    @IBAction func bt(_ sender: Any) {
+        lab.text = "Hello, " + labs.text!
+        labs.text = ""
+        labs.resignFirstResponder()
+        //view를 터치하면 키패드가 내려감
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        labs.resignFirstResponder()
+        view.backgroundColor = UIColor.black
+    }
+        //UITextFieldDelegate Method
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        labs.resignFirstResponder()
+        view.backgroundColor = UIColor.red
+        return true
+        
+    }
+        // clear button을 눌렀을때 호출
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        labs.backgroundColor = UIColor.darkGray
+        return true
+        
     }
     
-    @IBAction func bt(_ sender: Any) {
-        txt.text = " Hello " + txtField.text!
-        txtField.text = ""
-        txtField.resignFirstResponder() //
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
+
+
 }
